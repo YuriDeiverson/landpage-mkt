dddd123fd2f89ed40662a53e6f320ea50a595071
Yuri <121309922+YuriDeiverson@users.noreply.github.com>
2025-05-11 23:17:51 -0300

    Create script.js
    adicionando o js para sanduiche

---
+++ script.js
@@ -0,0 +1,8 @@
+<script>
+  const toggle = document.getElementById('menu-toggle');
+  const nav = document.getElementById('nav-bar');
+
+  toggle.addEventListener('click', () => {
+    nav.classList.toggle('show');
+  });
+</script>
