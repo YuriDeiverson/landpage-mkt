d54eb70432320f6dd9e06247a96bbb595d38a0f9
Yuri <121309922+YuriDeiverson@users.noreply.github.com>
2025-05-11 23:32:24 -0300

    Update script.js

--- script.js
+++ script.js
@@ -2,7 +2,8 @@
   const toggle = document.getElementById('menu-toggle');
   const nav = document.getElementById('nav-bar');
 
-  toggle.addEventListener('click', () => {
+  toggle.addEventListener('click', function () => {
     nav.classList.toggle('show');
   });
+});
 </script>
